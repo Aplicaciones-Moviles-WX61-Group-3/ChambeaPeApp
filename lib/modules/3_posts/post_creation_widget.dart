04650b541b79f7fab4dd35bb6db05098ec557d1e
Diego Castro <diego_castro_soto@hotmail.com>
2024-04-30 04:02:58 -0500

    feat: add post creation details step

--- lib/modules/3_posts/post_creation_widget.dart
+++ lib/modules/3_posts/post_creation_widget.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class PostCreationWidget extends StatefulWidget {
   const PostCreationWidget({super.key});
@@ -19,7 +22,7 @@ class _PostCreationWidgetState extends State<PostCreationWidget> {
         child:   Theme(data: ThemeData(
                   canvasColor: Colors.white,
                   primarySwatch: Colors.orange,
-                  colorScheme: const ColorScheme.dark(
+                  colorScheme: const ColorScheme.light(
                   primary: Colors.orange,
                   secondary: Colors.orange,
                   background: Colors.grey
@@ -126,15 +129,180 @@ class DetailsStepWidget extends StatefulWidget {
 }
 
 class _DetailsStepWidgetState extends State<DetailsStepWidget> {
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController retributionController = TextEditingController();
+  String dropdownCurrValue = 'Jardinería';
+
+  File? selectedImage;
+  Future getImage() async{
+    final pickedImage = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedImage != null) {
+        selectedImage = File(pickedImage.path);
+      } else {
+        const Text("Selecciona una imagen");
+      }
+    });
+  }
+
+  List<String> dropdownItems = [
+      'Jardinería',
+      'Educación',
+      'Música',
+      'Reparación de electrodomésticos',
+      'Fontanería',
+      'Carpintería',
+      'Limpieza doméstica',
+      'Pintura',
+      'Diseño gráfico',
+      'Consultoría financiera',
+      'Traducción e interpretación',
+      'Fotografía',
+      'Entrenamiento personal',
+      'Masajes terapéuticos',
+      'Cuidado de mascotas',
+      'Coaching',
+      'Cocina',
+      'Otro',
+    ];
+  
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-        padding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 0.0),
+    return Padding(
+        padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 0.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text("Detalles de la publicacion", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-            SizedBox(height: 10,)
+            const Text("Detalles de la publicación", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            const SizedBox(height: 10,),
+            TextField(
+              controller: titleController,
+              style: const TextStyle(color: Colors.black),
+              decoration: InputDecoration(
+                labelText: 'Titulo',
+                labelStyle: const TextStyle(color: Colors.black),
+                contentPadding: const EdgeInsets.fromLTRB(10.0, 12.0, 10.0, 12.0),
+                border: const OutlineInputBorder(),
+                fillColor: Colors.white,
+                filled: true,
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.amber.shade700),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.amber.shade700),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10,),
+            SizedBox(
+              height: 60,
+              width: double.infinity,
+              child: DropdownButtonFormField(
+                hint: const Text('Selecciona una categoría'),
+                icon: const Icon(Icons.arrow_drop_down),
+                iconSize: 36,
+                value: dropdownCurrValue,
+                isExpanded: true,
+                menuMaxHeight: 300,
+                items: dropdownItems.map((dropdownItem) {
+                  return DropdownMenuItem(
+                    value: dropdownItem,
+                    child: Text(dropdownItem));
+                }).toList(), 
+                onChanged:(value) {
+                  setState(() {
+                    dropdownCurrValue = value!;
+                  }); 
+                },
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 16
+                ),
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+                  fillColor: Colors.white,
+                  filled: true,
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.amber.shade700),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.amber.shade700),
+                  )
+                ),
+              ),
+            ),
+            TextField(
+              maxLines: null,
+              keyboardType: TextInputType.multiline,
+              minLines: 5,
+              decoration: InputDecoration(
+                alignLabelWithHint: true,
+                labelText: 'Descripción',
+                labelStyle: const TextStyle(color: Colors.black,),
+                contentPadding: const EdgeInsets.fromLTRB(10.0, 12.0, 10.0, 12.0),
+                border: const OutlineInputBorder(),
+                fillColor: Colors.white,
+                filled: true,
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.amber.shade700),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.amber.shade700),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10,),
+            TextField(
+              controller: retributionController,
+              style: const TextStyle(color: Colors.black),
+              decoration: InputDecoration(
+                labelText: 'Remuneración',
+                labelStyle: const TextStyle(color: Colors.black),
+                prefix: const Text("S/ "),
+                contentPadding: const EdgeInsets.fromLTRB(10.0, 12.0, 10.0, 12.0),
+                border: const OutlineInputBorder(),
+                fillColor: Colors.white,
+                filled: true,
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.amber.shade700),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.amber.shade700),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10,),
+            Container(
+              height: 100,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                color: Colors.grey.shade300,
+                borderRadius: BorderRadius.circular(5) 
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  children: [
+                    selectedImage != null ? Image.file(selectedImage!) : const Text("Por favor, selecciona una imagen"),
+                    IconButton(
+                      onPressed:() {
+                            setState(() {
+                              getImage();
+                            });
+                      },
+                      icon: const Icon(Icons.add),
+                    ),
+                  ],
+                ),
+              ),
+              
+            ),
+              const SizedBox(height: 100,),
+              // 
+
           ],
         ),
       );
