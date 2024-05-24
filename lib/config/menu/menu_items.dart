0dce978c7843adc19b4a16b0cdb6d48168cc2940
steve <stev8855@gmail.com>
2024-05-23 21:06:53 -0500

    feat: add menu items to custom navigation bar

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -12,4 +12,30 @@ class MenuItem {
   });
 }
 
-const appMenuItems = <MenuItem>[];
+const appMenuItems = <MenuItem>[
+  MenuItem(
+    title: 'Inicio',
+    link: '/',
+    icon: Icons.home
+  ),
+  MenuItem(
+    title: 'Posts',
+    link: '/posts',
+    icon: Icons.post_add
+  ),
+  MenuItem(
+    title: 'Chat',
+    link: '/chat',
+    icon: Icons.chat
+  ),
+  MenuItem(
+    title: 'Workers',
+    link: '/workers',
+    icon: Icons.work
+  ),
+  MenuItem(
+    title: 'Perfil',
+    link: '/profile',
+    icon: Icons.person
+  )
+];
