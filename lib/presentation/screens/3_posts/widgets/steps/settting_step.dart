7d8ccfcb0d125da78142fe89b5de43fd335f1570
steve <stev8855@gmail.com>
2024-05-26 15:30:52 -0500

    feat: add setting step

---
+++ lib/presentation/screens/3_posts/widgets/steps/settting_step.dart
@@ -0,0 +1,78 @@
+import 'package:chambeape/config/utils/login_user_data.dart';
+import 'package:chambeape/infrastructure/models/login/login_response.dart';
+import 'package:chambeape/presentation/providers/posts/post_provider.dart';
+import 'package:chambeape/presentation/providers/posts/steps/step_provider.dart';
+import 'package:chambeape/presentation/shared/widgets/premium_dialog.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+class SettingStep extends ConsumerStatefulWidget {
+  const SettingStep({
+    super.key,
+  });
+
+  @override
+  createState() => _SetttingStepState();
+}
+
+class _SetttingStepState extends ConsumerState<SettingStep> {
+  late LoginResponse user;
+  bool hasPremium = false;
+
+  @override
+  void initState() {
+    super.initState();
+    ref.read(postsProvider.notifier).getPosts();
+    LoginData().loadSession().then((value) {
+      setState(() {
+        user = value;
+        hasPremium = user.hasPremium == 0 ? false : true;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final stepperPostProv = ref.watch(stepperPostProvider.notifier);
+    final text = Theme.of(context).textTheme;
+
+    bool resPremium = stepperPostProv.hasPremium == 0 ? false : true;
+
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Text('Configuración', style: text.headlineSmall),
+          const SizedBox(height: 16),
+          SwitchListTile(
+            title: Text('Destacar', style: text.headlineSmall),
+            subtitle: Text('Función premium para destacar la publicación', style: text.bodySmall),
+            value: resPremium,
+            onChanged: (value) {
+              print(user.hasPremium);
+              if (hasPremium == false) {
+                showDialog(
+                  context: context,
+                  builder: (context) => const PremiumDialog(),
+                );
+              } else {
+                setState(() {
+                  stepperPostProv.setPremium(value);
+                });
+              }
+            },
+          ),
+          const SizedBox(height: 16),
+          SwitchListTile(
+            title: Text('Notificaciones', style: text.headlineSmall),
+            subtitle: Text('Recibir notificaciones de la publicación', style: text.bodySmall),
+            value: stepperPostProv.hasNotification,
+            onChanged: (notification) {
+              setState(() {
+                stepperPostProv.setNotification(notification);
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
