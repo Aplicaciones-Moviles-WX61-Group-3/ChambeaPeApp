d7e3b91a061eccd831c6209d3dc81577abad33c9
steve <stev8855@gmail.com>
2024-05-25 22:19:51 -0500

    refactor: update StepperPost to use StepperPostNotifier for managing state and data

--- lib/presentation/screens/3_posts/widgets/stepper_post.dart
+++ lib/presentation/screens/3_posts/widgets/stepper_post.dart
@@ -1,17 +1,21 @@
 import 'package:chambeape/domain/entities/posts_entity.dart';
+import 'package:chambeape/presentation/providers/posts/steps/step_provider.dart';
+import 'package:chambeape/presentation/screens/3_posts/widgets/steps/details_step.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class StepperPost extends StatefulWidget {
-  final Post? post; // Hacer el parámetro post opcional
+class StepperPost extends ConsumerStatefulWidget {
+  final Post? post;
 
   const StepperPost({super.key, this.post});
 
   @override
-  State<StepperPost> createState() => _StepperPostState();
+  createState() => _StepperPostState();
 }
 
-class _StepperPostState extends State<StepperPost> {
+class _StepperPostState extends ConsumerState<StepperPost> {
   bool hasPost = false;
+  int currStep = 0;
 
   @override
   void initState() {
@@ -21,40 +25,96 @@ class _StepperPostState extends State<StepperPost> {
 
   @override
   Widget build(BuildContext context) {
+    final stepperPostProv = ref.watch(stepperPostProvider.notifier);
+
+    const totalSteps = 4;
     return Scaffold(
       appBar: AppBar(
-        title: Text(hasPost ? 'Edit Post' : 'Create Post'),
+        title: Text(hasPost ? 'Editar post' : 'Crear post'),
       ),
       body: Stepper(
-        currentStep: 0,
+        type: StepperType.horizontal,
+        currentStep: currStep,
+        onStepContinue: () async {
+          // Validar el formulario antes de pasar al siguiente paso
+          final form = stepperPostProv.formKey.currentState;
+          if (form != null && form.validate()) {
+            if (currStep < totalSteps - 1) {
+              setState(() {
+                currStep += 1;
+              });
+            } else {
+              // Guardar el post
+              print('Guardando post...');
+              final data = stepperPostProv.getAll();
+              print(data);
+            }
+          }
+        },
+        onStepCancel: () {
+          if (currStep > 0) {
+            setState(() {
+              currStep -= 1;
+            });
+          }
+        },
         steps: [
           Step(
-            title: const Text('Step 1'),
-            content: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Title'),
-                  initialValue: hasPost ? widget.post!.title : '',
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Description'),
-                  initialValue: hasPost ? widget.post!.description : '',
-                ),
-              ],
-            ),
+            title: const SizedBox.shrink(),
+            content: DetailsStepWidget(hasPost: hasPost, widget: widget),
+            isActive: currStep >= 0,
           ),
           Step(
-            title: const Text('Step 2'),
+            title: const SizedBox.shrink(),
             content: Column(
               children: [
                 TextFormField(
                   decoration: const InputDecoration(labelText: 'Image URL'),
-                  initialValue: hasPost ? widget.post!.imgUrl : '',
+                  initialValue: hasPost ? widget.post?.imgUrl : '',
                 ),
               ],
             ),
+            isActive: currStep >= 1,
+          ),
+          // Añadir más pasos según sea necesario
+          Step(
+            title: const SizedBox.shrink(),
+            content: const Text('Contenido del paso 3'),
+            isActive: currStep >= 2,
+          ),
+          Step(
+            title: const SizedBox.shrink(),
+            content: const Text('Contenido del paso 4'),
+            isActive: currStep >= 3,
           ),
         ],
+        controlsBuilder: (context, details) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              if (currStep > 0)
+                TextButton(
+                  onPressed: details.onStepCancel,
+                  child: const Text('Volver'),
+                ),
+              if (currStep < totalSteps - 1)
+                TextButton(
+                  onPressed: details.onStepContinue,
+                  child: const Text('Siguiente'),
+                ),
+              if (currStep == totalSteps - 1)
+                TextButton(
+                  onPressed: () {
+                    // Guardar el post
+                    print('Guardando post...');
+                    final data = stepperPostProv.getAll();
+                    print(data);
+                  },
+                  child: const Text('Guardar'),
+                ),
+            ],
+          );
+        },
       ),
     );
   }
