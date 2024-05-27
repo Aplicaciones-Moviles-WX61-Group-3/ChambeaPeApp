c117384889bce41819ab364651cb94a216e13aa9
steve <stev8855@gmail.com>
2024-05-26 22:03:20 -0500

    refactor: consolidate step imports in StepperPost using StepBarrel widget

--- lib/presentation/providers/posts/steps/step_provider.dart
+++ lib/presentation/providers/posts/steps/step_provider.dart
@@ -1,58 +1,11 @@
 import 'dart:io';
+import 'package:chambeape/presentation/providers/posts/steps/stepper_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final stepperPostProvider = StateNotifierProvider<StepperPostNotifier, StepperPostState>((ref) {
   return StepperPostNotifier();
 });
-
-class StepperPostState {
-  final TextEditingController titleController;
-  final TextEditingController descriptionController;
-  final TextEditingController categoryController;
-  final TextEditingController locationController;
-  final bool hasNotification;
-  final int hasPremium;
-  final File? selectedImage;
-  final GlobalKey<FormState> formKeyPostDetails;
-  final GlobalKey<FormState> formKeyPostLocation;
-
-  StepperPostState({
-    required this.titleController,
-    required this.descriptionController,
-    required this.categoryController,
-    required this.locationController,
-    this.hasNotification = false,
-    this.hasPremium = 0,
-    this.selectedImage,
-    required this.formKeyPostDetails,
-    required this.formKeyPostLocation,
-  });
-
-  StepperPostState copyWith({
-    TextEditingController? titleController,
-    TextEditingController? descriptionController,
-    TextEditingController? categoryController,
-    TextEditingController? locationController,
-    bool? hasNotification,
-    int? hasPremium,
-    File? selectedImage,
-    GlobalKey<FormState>? formKeyPostDetails,
-    GlobalKey<FormState>? formKeyPostLocation,
-  }) {
-    return StepperPostState(
-      titleController: titleController ?? this.titleController,
-      descriptionController: descriptionController ?? this.descriptionController,
-      categoryController: categoryController ?? this.categoryController,
-      locationController: locationController ?? this.locationController,
-      hasNotification: hasNotification ?? this.hasNotification,
-      hasPremium: hasPremium ?? this.hasPremium,
-      selectedImage: selectedImage ?? this.selectedImage,
-      formKeyPostDetails: formKeyPostDetails ?? this.formKeyPostDetails,
-      formKeyPostLocation: formKeyPostLocation ?? this.formKeyPostLocation,
-    );
-  }
-}
 
 class StepperPostNotifier extends StateNotifier<StepperPostState> {
   StepperPostNotifier()
