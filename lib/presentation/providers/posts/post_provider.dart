aa5e9c30220398b497d9de5f12d1c51a40d263a1
steve <stev8855@gmail.com>
2024-05-28 00:15:39 -0500

    refactor: Add create, update, and delete methods to PostsNotifier

--- lib/presentation/providers/posts/post_provider.dart
+++ lib/presentation/providers/posts/post_provider.dart
@@ -3,23 +3,39 @@ import 'package:chambeape/presentation/providers/posts/posts_repository_provider
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final postsProvider = StateNotifierProvider<PostsNotifier, List<Post>>((ref) {
-  final repository = ref.watch(postsRepositoryProvider).getPosts;
+  final repository = ref.watch(postsRepositoryProvider);
 
   return PostsNotifier(
-    fetchPosts: repository,
+    fetchPosts: repository.getPosts,
+    createPostCallback: repository.createPost,
+    updatePostCallback: repository.updatePost,
+    deletePostCallback: repository.deletePost,
   );
 });
 
-typedef PostsCallback = Future<List<Post>> Function();
+typedef GetPostsCallback = Future<List<Post>> Function();
+typedef CreatePostCallback = Future<Post> Function(Post post);
+typedef UpdatePostCallback = Future<Post> Function(Post post);
+typedef DeletePostCallback = Future<void> Function(String id);
+
 
 class PostsNotifier extends StateNotifier<List<Post>> {
   bool isLoading = false;
+  bool _isDeleting = false;
 
-  PostsCallback fetchPosts;
+  final GetPostsCallback fetchPosts;
+  final CreatePostCallback createPostCallback;
+  final UpdatePostCallback updatePostCallback;
+  final DeletePostCallback deletePostCallback;
 
   PostsNotifier({
     required this.fetchPosts,
+    required this.createPostCallback,
+    required this.updatePostCallback,
+    required this.deletePostCallback,
   }) : super([]);
+
+  bool get isDeleting => _isDeleting;
 
   Future<void> getPosts() async {
     if (isLoading) return;
@@ -27,7 +43,35 @@ class PostsNotifier extends StateNotifier<List<Post>> {
     isLoading = true;
     final posts = await fetchPosts();
     state = posts;
-
     isLoading = false;
+  }
+
+  Future<void> createPost(Post post) async {
+    if (isLoading) return;
+
+    isLoading = true;
+    final newPost = await createPostCallback(post);
+    state = [...state, newPost];
+    isLoading = false;
+  }
+
+  Future<void> updatePost(Post post) async {
+    if (isLoading) return;
+
+    isLoading = true;
+    final updatedPost = await updatePostCallback(post);
+    state = state.map((p) => p.id == updatedPost.id ? updatedPost : p).toList();
+    isLoading = false;
+  }
+
+  Future<void> deletePost(String id) async {
+    if (_isDeleting) return;
+
+    _isDeleting = true;
+    state = [...state];
+    await deletePostCallback(id);
+    state = state.where((p) => p.id != int.parse(id)).toList();
+    _isDeleting = false;
+    state = [...state];
   }
 }
