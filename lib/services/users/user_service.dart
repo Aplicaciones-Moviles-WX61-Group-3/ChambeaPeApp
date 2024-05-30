c230e7951e5e4dd387f11cb901830c7eb66fc225
Jennifer Espinoza <jenniespinoza2002@hotmail.com>
2024-05-30 16:40:18 -0500

    feat: add post method to user service

--- lib/services/users/user_service.dart
+++ lib/services/users/user_service.dart
@@ -3,19 +3,34 @@ import 'dart:convert';
 import 'package:chambeape/infrastructure/models/users.dart';
 import 'package:http/http.dart' as http;
 
-class UserService{
+class UserService {
   final uri = Uri.parse('https://chambeape.azurewebsites.net/api/v1/users');
 
-  Future<List<Users>> getUsers() async{
+  Future<List<Users>> getUsers() async {
     final response = await http.get(uri);
 
     if (response.statusCode >= 200 && response.statusCode < 300) {
       List<dynamic> body = json.decode(utf8.decode(response.bodyBytes));
-      List<Users> users = body.map((dynamic item) => Users.fromJson(item)).toList();
+      List<Users> users =
+          body.map((dynamic item) => Users.fromJson(item)).toList();
       return users;
+    } else {
+      throw Exception(
+          'Failed to fetch users: Status Code ${response.statusCode}, Response Body: ${response.body}');
     }
-    else {
-      throw Exception('Failed to fetch users: Status Code ${response.statusCode}, Response Body: ${response.body}');
+  }
+
+  Future<Users> postUser(Users user) async {
+    final response = await http.post(uri,
+        body: json.encode(user.toJson()),
+        headers: {'Content-Type': 'application/json'});
+
+    if (response.statusCode >= 200 && response.statusCode < 300) {
+      print('User posted successfully');
+      return Users.fromJson(json.decode(utf8.decode(response.bodyBytes)));
+    } else {
+      throw Exception(
+          'Failed to post user: Status Code ${response.statusCode}, Response Body: ${response.body}');
     }
   }
 }
