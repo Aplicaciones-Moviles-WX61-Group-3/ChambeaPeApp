9daf1cb29f4bd80a65aa99b77ac04afee6eabca5
steve <stev8855@gmail.com>
2024-05-31 11:02:41 -0500

    refactor: Add birthdate, gender, and dni fields to Employers and Workers models

--- lib/infrastructure/models/workers.dart
+++ lib/infrastructure/models/workers.dart
@@ -1,58 +1,63 @@
 import 'package:chambeape/infrastructure/models/users.dart';
 
 class Workers {
-  final int id;
-  final String firstName;
-  final String lastName;
-  final String email;
-  final String phoneNumber;
-  final int hasPremium;
-  final String profilePic;
-  final int isActive;
-  final String description;
-  final String occupation;
+    final int id;
+    final String firstName;
+    final String lastName;
+    final String email;
+    final String phoneNumber;
+    final DateTime birthdate;
+    final String gender;
+    final String dni;
+    final String profilePic;
+    final String description;
+    final String userRole;
+    final int hasPremium;
 
-  Workers({
-    required this.id,
-    required this.firstName,
-    required this.lastName,
-    required this.email,
-    required this.phoneNumber,
-    required this.hasPremium,
-    required this.profilePic,
-    required this.isActive,
-    required this.description,
-    required this.occupation,
-  });
+    Workers({
+        required this.id,
+        required this.firstName,
+        required this.lastName,
+        required this.email,
+        required this.phoneNumber,
+        required this.birthdate,
+        required this.gender,
+        required this.dni,
+        required this.profilePic,
+        required this.description,
+        required this.userRole,
+        required this.hasPremium,
+    });
 
-  factory Workers.fromJson(Map<String, dynamic> json) {
-    return Workers(
-      id: json['id'],
-      firstName: json['firstName'],
-      lastName: json['lastName'],
-      email: json['email'],
-      phoneNumber: json['phoneNumber'],
-      hasPremium: json['hasPremium'],
-      profilePic: json['profilePic'],
-      isActive: json['isActive'],
-      description: json['description'],
-      occupation: json['occupation'],
+    factory Workers.fromJson(Map<String, dynamic> json) => Workers(
+        id: json["id"],
+        firstName: json["firstName"],
+        lastName: json["lastName"],
+        email: json["email"],
+        phoneNumber: json["phoneNumber"],
+        birthdate: DateTime.parse(json["birthdate"]),
+        gender: json["gender"],
+        dni: json["dni"],
+        profilePic: json["profilePic"],
+        description: json["description"],
+        userRole: json["userRole"],
+        hasPremium: json["hasPremium"]
     );
-  }
 
-  Map<String, dynamic> toJson() => {
-        'id': id,
-        'firstName': firstName,
-        'lastName': lastName,
-        'email': email,
-        'phoneNumber': phoneNumber,
-        'hasPremium': hasPremium,
-        'profilePic': profilePic,
-        'isActive': isActive,
-        'description': description,
-        'occupation': occupation,
-      };
-
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "firstName": firstName,
+        "lastName": lastName,
+        "email": email,
+        "phoneNumber": phoneNumber,
+        "birthdate": birthdate.toIso8601String(),
+        "gender": gender,
+        "dni": dni,
+        "profilePic": profilePic,
+        "description": description,
+        "userRole": userRole,
+        "hasPremium": hasPremium,
+    };
   Users toUser() {
     return Users(
       id: id,
@@ -66,6 +71,7 @@ class Workers {
       description: description,
       userRole: 'W',
       dni: '',
+      hasPremium: 0,
     );
   }
 }
